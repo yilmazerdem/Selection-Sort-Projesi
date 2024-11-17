e12d2c877b11cdb9e93c8e948de569addf917148
Erdem Yılmaz <36715898+yilmazerdem@users.noreply.github.com>
2024-11-17 20:11:08 +0300

    Add files via upload
    patika bootcamp

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace dotnetprogramlama
+{
+    class Program
+    {
+    public static void Main(string[] args)
+    {
+     
+ // See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("İsminizi giriniz");
+string name =Console.ReadLine();
+Console.WriteLine("Soyadınızı giriniz");      
+string surname =Console.ReadLine();
+
+Console.WriteLine("Merhabalar > "+name+" "+surname);
+    }
+    }
+}
